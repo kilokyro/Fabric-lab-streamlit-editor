ba50dd082eb557f9921844d510cad15fe25005f1
Alex Huang <cshuangtw@gmail.com>
2023-10-04 12:27:58 +0000

    A

--- workdir/org1-client/scripts/setAnchorPeer.sh
+++ workdir/org1-client/scripts/setAnchorPeer.sh
@@ -11,4 +11,5 @@ configtxlator compute_update --channel_id $CHANNEL_NAME --original config.pb --u
 configtxlator proto_decode --input config_update.pb --type common.ConfigUpdate --output config_update.json
 echo '{"payload":{"header":{"channel_header":{"channel_id":"'$CHANNEL_NAME'", "type":2}},"data":{"config_update":'$(cat config_update.json)'}}}'|jq . > config_update_in_envelope.json
 configtxlator proto_encode --input config_update_in_envelope.json --type common.Envelope --output config_update_in_envelope.pb
-peer channel update -f config_update_in_envelope.pb -c $CHANNEL_NAME -o orderer1.org4.com:4150 --tls --cafile $ORDERER_TLS_CA
+
+peer channel update -f config_update_in_envelope.pb -c $CHANNEL_NAME -o orderer0.org4.com:4050 --tls --cafile $ORDERER_TLS_CA
